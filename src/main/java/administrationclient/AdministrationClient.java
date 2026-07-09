21bd1a57c75b300b4cba3989fcb8dacb383b0479
Auste <auste.zuklyte@gmail.com>
2026-07-09 06:15:50 +0300

    Update Administration Client to use java HttpClient instead of Jersey Client

--- src/main/java/administrationclient/AdministrationClient.java
+++ src/main/java/administrationclient/AdministrationClient.java
@@ -1,8 +1,7 @@
-package Administration_Client;
+package administrationclient;
 
 import Extensions.IntegerExtension;
 import Extensions.LongExtension;
-import io.opencensus.common.ServerStatsFieldEnums;
 
 import java.util.Scanner;
 
