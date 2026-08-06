28116e186bbf3721d4333b97cdbd33fd73d28266
Auste <auste.zuklyte@gmail.com>
2026-08-07 01:17:56 +0300

    Paths in admin clients connection to admin server

--- src/main/java/administration_client/client/AdminServerClient.java
+++ src/main/java/administration_client/client/AdminServerClient.java
@@ -19,19 +19,27 @@ import java.util.logging.Logger;
 
 public class AdminServerClient {
     private final static Logger LOGGER = Logger.getLogger(AdminServerClient.class.getName());
+    private final static String GET_PLAYERS_PATH = "/players/list";
+    private final static String ANALYTICS_BETWEEN_PATH = "/analytics/getvaluesbetweentimestamps";
+    private final static String ANALYTICS_LAST_PATH = "/analytics/getlastnmeasurements/{playerId}/{count}";
 
-    private final String baseUrl;
+
+    private final String getPlayersUrl;
+    private final String analyticsBetweenUrl;
+    private final String analyticsLastUrl;
     private final HttpClient httpClient;
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
     public AdminServerClient(String url, HttpClient httpClient) {
-        this.baseUrl = url;
+        this.getPlayersUrl = url + GET_PLAYERS_PATH;
+        this.analyticsBetweenUrl = url + ANALYTICS_BETWEEN_PATH;
+        this.analyticsLastUrl = url + ANALYTICS_LAST_PATH;
         this.httpClient = httpClient;
     }
 
     public List<PlayerInfo> getAllPlayers() {
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(UriBuilder.fromUri(baseUrl).build())
+                .uri(URI.create(getPlayersUrl))
                 .header("Accept", "application/json")
                 .GET()
                 .build();
@@ -65,7 +73,7 @@ public class AdminServerClient {
             String jsonBody = objectMapper.writeValueAsString(timestampsDto);
 
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(baseUrl + "/analytics/getvaluesbetweentimestamps"))
+                    .uri(URI.create(analyticsBetweenUrl))
                     .header("Accept", "application/json")
                     .header("Content-Type", "application/json")
                     .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
@@ -98,8 +106,13 @@ public class AdminServerClient {
 
     public double getLatestMeasurementAverage(int playerId, int count) {
         try {
+            URI uri = UriBuilder.fromUri(analyticsLastUrl)
+                    .resolveTemplate("playerId", playerId)
+                    .resolveTemplate("count", count)
+                    .build();
+
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(baseUrl + "/analytics/getlastnmeasurements/" + playerId + "/" + count))
+                    .uri(uri)
                     .header("Accept", "application/json")
                     .GET()
                     .build();
